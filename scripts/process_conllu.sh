f66a0dcbfcb1a4df7ce888316bbb2a767787924c
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2020-03-02 13:49:43 +0100

    Update conllu processing script

--- scripts/process_conllu.sh
+++ scripts/process_conllu.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+# Args
+if [ ! $# -eq 4 ]; then
+  echo Usage: `basename $0` 'MODEL' 'INPUT-DIR' 'TMP-DIR' 'OUTPUT-DIR'
+  echo
+  exit
+fi
+
+UDPIPE_PL=$1
+# =udpipe/models/polish-pdb-ud-2.5-191206.udpipe
+INP=$2
+TMP=$3
+OUT=$4
+
 # Echo on
 set -x
 
-# Paths
-DATA=data
-UDPIPE_PL=udpipe/models/polish-pdb-ud-2.5-191206.udpipe
-INP=...
-TMP=...
-OUT=...
-
-for xz_file in $DATA/$INP/*.conllu.xz
+for xz_file in $INP/*.conllu.xz
 do
   xz_file="$(basename -- $xz_file)"
-  cp $DATA/$INP/$xz_file $DATA/$TMP/inp.conllu.xz
-  unxz $DATA/$TMP/inp.conllu.xz
-  python3 main.py parse --raw -i $DATA/$TMP/inp.conllu -m $DATA/$UDPIPE_PL > $DATA/$TMP/out.conllu
-  xz $DATA/$TMP/out.conllu
-  mv $DATA/$TMP/out.conllu.xz $DATA/$OUT/$xz_file
+  cp $INP/$xz_file $TMP/inp.conllu.xz
+  unxz $TMP/inp.conllu.xz
+  python3 main.py parse --raw -i $TMP/inp.conllu -m $UDPIPE_PL > $TMP/out.conllu
+  xz $TMP/out.conllu
+  mv $TMP/out.conllu.xz $OUT/$xz_file
 done
